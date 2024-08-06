4a05aa96093c6740e468d2812b2fe43acfb11852
duttaaindrila23 <aindriladutta392@gmail.com>
2024-08-07 02:53:51 +0530

    Airbnb Clone

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#FFC6C6'
+      },
+      fontFamily:
+      {
+        'myfont':['Roboto', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
